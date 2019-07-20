2a3dde2c85fc19e54dec40e8480d30bb84b002b5
Kandarp Vyas <52797390+KandarpXRaiden@users.noreply.github.com>
2019-07-20 10:29:42 +0530

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,12 @@
+public class Convert{
+	public static void main(String args[])
+	{
+	System.out.println("This is a program for conversion from rupees to dollar.");
+	double rupee=1000,dollar;
+	System.out.println("Enter the rupees:-\n");
+	dollar = rupee/68.85;
+	System.out.println("Conversion is :- "+dollar);
+	}
+}
+	
+	
