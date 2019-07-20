ab4b0f4322ec4b654690fc4ab625c85c9937843a
Kandarp Vyas <52797390+KandarpXRaiden@users.noreply.github.com>
2019-07-20 10:31:01 +0530

    Update Convert.java

--- Convert.java
+++ Convert.java
@@ -2,7 +2,7 @@ public class Convert{
 	public static void main(String args[])
 	{
 	System.out.println("This is a program for conversion from rupees to dollar.");
-	double rupee=1000,dollar;
+	double rupee=1000,dollar;//keep in mind that if you want to take input from usefr you will have to use Scanner method//
 	System.out.println("Enter the rupees:-\n");
 	dollar = rupee/68.85;
 	System.out.println("Conversion is :- "+dollar);
