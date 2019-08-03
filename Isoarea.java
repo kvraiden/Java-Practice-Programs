01866f892e123c3d1882c60c6116e79dc2ddd59d
Kandarp Vyas <52797390+KandarpXRaiden@users.noreply.github.com>
2019-08-03 22:57:36 +0530

    Add files via upload

---
+++ Isoarea.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Isoarea{
+	public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);//Here System.in means we r gonna take Input from user console//
+	//Lets write main program//
+	//Isosceceles area=B/4√((4*a*a)-(b*b)) B is base 2 angles are same
+	System.out.println("Enter the length of same sides:- ");
+	double a= s.nextDouble();
+	System.out.println("Enter the length of base");
+	double b= s.nextDouble();
+	//Main logic
+	double area= ((b/4)*Math.sqrt((4*a*a)-(b*b)));
+	System.out.println("Area of special triangle is:- "+area);
+}
+}	
+	
